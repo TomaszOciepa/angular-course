8a93251a59ea4339865e550060794976d006a731
tomszociepa <tom.ociepa@gmail.com>
2023-12-22 13:01:18 +0100

    derektywa ngFor

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Todo } from '../shared/interfaces/todo.interface';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent {
+
+  todos: Todo[] = [];
+
+  addTodo(todo: string): void{
+    if(todo.length <= 3){
+      alert('Zadanie powinno mieć co najmniej 4 znaki.');
+      return;
+    }
+
+    this.todos.push({ name: todo, isComplete: false});
+    console.log('Aktualna lista todo:', this.todos);
+  }
+
+}
