2627ac66e006f0f41091d095037e4a9107975d2b
tomszociepa <tom.ociepa@gmail.com>
2023-12-23 12:17:44 +0100

    wydzielenie czesci kodu add-todo-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HeaderComponent } from './header/header.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { FormsModule } from '@angular/forms';
 import { AlertComponent } from './shared/components/alert/alert.component';
+import { AddTodoFormComponent } from './todo-list/add-todo-form/add-todo-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     TodoListComponent,
-    AlertComponent
+    AlertComponent,
+    AddTodoFormComponent
   ],
   imports: [
     BrowserModule,
