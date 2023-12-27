ce912fd11acb84a68f485b1d33a463a362539344
tomszociepa <tom.ociepa@gmail.com>
2023-12-27 21:24:45 +0100

    L92 komunikaty o bledach 2 sposob

--- src/app/todo-list/add-todo-form/add-todo-form.component.ts
+++ src/app/todo-list/add-todo-form/add-todo-form.component.ts
@@ -1,25 +1,18 @@
-import { NgFor } from '@angular/common';
-import { AfterViewInit, Component, EventEmitter, Output, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-todo-form',
   templateUrl: './add-todo-form.component.html',
   styleUrls: ['./add-todo-form.component.css']
 })
-export class AddTodoFormComponent implements AfterViewInit{
+export class AddTodoFormComponent{
 
   @Output() addTodo = new EventEmitter<string>();
-  @ViewChild('form') todoForm!: NgForm;
 
   todoName = '';
 
-  addNewTodo(form: NgForm){
-    console.log(form)
+  addNewTodo(){
     this.addTodo.emit(this.todoName);
-  }
-
-  ngAfterViewInit(): void {
-    console.log(this.todoForm)
   }
 }
