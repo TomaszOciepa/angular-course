454dbf1b4ae7e022a4dd33fe97321ad939390f88
tomszociepa <tom.ociepa@gmail.com>
2023-12-27 21:34:10 +0100

    L93 dyrektywa ngModelGroup

--- src/app/todo-list/add-todo-form/add-todo-form.component.ts
+++ src/app/todo-list/add-todo-form/add-todo-form.component.ts
@@ -12,7 +12,10 @@ export class AddTodoFormComponent{
 
   todoName = '';
 
+  person = {name: 'test', surname: 'testowy', role:'father'}
+
   addNewTodo(){
+    console.log(this.person)
     this.addTodo.emit(this.todoName);
   }
 }
