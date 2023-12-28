8b7a68284d824544dc18aaac5184baeccb8ff673
tomszociepa <tom.ociepa@gmail.com>
2023-12-28 10:47:12 +0100

    L98 definiowanie ścieżek

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ModalComponent } from './shared/components/modal/modal.component';
 import localePl from '@angular/common/locales/pl'
 import { registerLocaleData } from '@angular/common';
 import { FirstLetterDirective } from './shared/validators/first-letter.directive';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 registerLocaleData(localePl)
 
@@ -25,7 +27,9 @@ registerLocaleData(localePl)
     AddTodoFormComponent,
     TodoComponent,
     ModalComponent,
-    FirstLetterDirective
+    FirstLetterDirective,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
