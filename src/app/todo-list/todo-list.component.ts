d132b000119160053030f3f8494c8373dc8ffc12
tomszociepa <tom.ociepa@gmail.com>
2024-01-05 10:56:18 +0100

    L217 Zmiany w pierwszym projekcie (lista zadań)

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -4,69 +4,69 @@ import { TodoService } from '../core/services/todo.service';
 import { Subscription } from 'rxjs';
 import { TodoApiService } from '../core/services/todo-api.service';
 
-
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent implements OnInit, OnDestroy{
-
-  todos: Todo[] = this.todoService.todos;
+export class TodoListComponent implements OnInit, OnDestroy {
+  // todos: Todo[] = this.todoService.todos;
+  todos: Todo[] = [];
 
   errorMessages = '';
 
   sub!: Subscription;
 
-  constructor(private todoService: TodoService, private todoApiService: TodoApiService){}
+  constructor(
+    private todoService: TodoService,
+    private todoApiService: TodoApiService
+  ) {}
 
   ngOnInit(): void {
-    this.sub = this.todoService.todoChanged.subscribe({
-      next: arrTodos => this.todos = arrTodos
-    })  
-
-    if(this.todos.length === 0){
-      this.todoApiService.getTodos().subscribe({
-        error: err =>{
-          this.errorMessages = 'Wystąpił błąd spróbuj ponownie.'
-        }
-      })
-    }
-    
+    // this.sub = this.todoService.todoChanged.subscribe({
+    //   next: (arrTodos) => (this.todos = arrTodos),
+    // });
+    // if (this.todos.length === 0) {
+    //   this.todoApiService.getTodos().subscribe({
+    //     error: (err) => {
+    //       this.errorMessages = 'Wystąpił błąd spróbuj ponownie.';
+    //     },
+    //   });
+    // }
   }
-  
-  addTodo(todo: string): void{
-    this.todoApiService.postTodo({name: todo, isComplete: false}).subscribe({
-      error: err =>{
-        this.errorMessages = 'Wystąpił błąd spróbuj ponownie.'
-      }
-    })
-    
+
+  addTodo(todo: string): void {
+    // this.todoApiService.postTodo({name: todo, isComplete: false}).subscribe({
+    //   error: err =>{
+    //     this.errorMessages = 'Wystąpił błąd spróbuj ponownie.'
+    //   }
+    // })
     // this.todos = this.todoService.todos;
   }
 
-    clearErrorMessage(){
-      this.errorMessages = '';
-    }
+  clearErrorMessage() {
+    this.errorMessages = '';
+  }
 
-    deleteTodo(id: number){
-      this.todoApiService.deleteTodo(id).subscribe({
-        error: err =>{
-          this.errorMessages = 'Wystąpił błąd spróbuj ponownie.'
-        }
-      })
-    }
+  deleteTodo(id: number) {
+    // this.todoApiService.deleteTodo(id).subscribe({
+    //   error: (err) => {
+    //     this.errorMessages = 'Wystąpił błąd spróbuj ponownie.';
+    //   },
+    // });
+  }
 
-    changeToDoStatus(id: number, todo: Todo){
-      this.todoApiService.patchTodo(id, {isComplete: !todo.isComplete}).subscribe({
-       error: err => {
-        this.errorMessages = 'Wystąpił błąd spróbuj ponownie.'
-      }
+  changeToDoStatus(id: number, todo: Todo) {
+    // this.todoApiService
+    //   .patchTodo(id, { isComplete: !todo.isComplete })
+    //   .subscribe({
+    //     error: (err) => {
+    //       this.errorMessages = 'Wystąpił błąd spróbuj ponownie.';
+    //     },
+    //   });
+  }
 
-      })
-    }
-
-    ngOnDestroy(): void {
-      this.sub.unsubscribe();
-    }
+  ngOnDestroy(): void {
+    // this.sub.unsubscribe();
+  }
 }
